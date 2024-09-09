0c2bd62be5402e72e9843b3a7200a66e383f894c
Sushant2518 <sushantnarvekar3118@gmail.com>
2024-09-09 17:37:01 +0530

    added

---
+++ CSV_Operation/src/main/java/com/propescta/CsvOperationApplication.java
@@ -0,0 +1,13 @@
+package com.propescta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvOperationApplication.class, args);
+	}
+
+}
